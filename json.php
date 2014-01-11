f93dae43f5539a285cdb08f34295238ddda8a780
joekeyel <hasanul2307@gmail.com>
2014-01-12 05:09:04 +0800

    test3

---
+++ json.php
@@ -0,0 +1,54 @@
+<?php require_once('../../Connections/migration.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+mysql_select_db($database_migration, $migration);
+$query_schedule = "SELECT * FROM migration_schedule where mig_date > date(now())";
+$schedule = mysql_query($query_schedule, $migration) or die(mysql_error());
+$row_schedule = mysql_fetch_assoc($schedule);
+$totalRows_schedule = mysql_num_rows($schedule);
+
+
+
+$rows = array();
+
+
+?>
+<?php do { 
+ $rows[] = $row_schedule;
+ echo json_encode($rows);
+  ?> 
+  <?php } while ($row_schedule = mysql_fetch_assoc($schedule)); 
+  
+  
+  mysql_free_result($schedule);
+  ?>
